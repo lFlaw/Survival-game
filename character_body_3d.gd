393c8343084a02b746253ccdbb529115a458e687
Ritsu <silhavikmarko57@gmail.com>
2026-02-25 11:40:52 +0100

    init

---
+++ character_body_3d.gd
@@ -0,0 +1,76 @@
+extends CharacterBody3D
+
+var speed
+const sprint_speed = 8.0
+const walk_speed = 5.0
+const jump_velocity = 4.5
+const sensitivity = 0.03
+
+#viewbob
+const bob_freq = 2.0
+const bob_amp = 0.05
+var t_bob = 0.0
+
+#fov stuff
+const base_fov = 75.0
+const fov_change = 1.5
+
+@onready var head = $head
+@onready var camera = $head/Camera3D
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+	
+	#handle sprint speed stuff
+	if Input.is_action_pressed("sprint"):
+		speed = sprint_speed
+	else:
+		speed = walk_speed
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "forward", "backwards")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, speed)
+			velocity.z = move_toward(velocity.z, 0, speed)
+	else:
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
+		velocity.z = lerp(velocity.x, direction.x * speed, delta * 3.0)
+	#head bob stuff
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	camera.transform.origin = _headbob(t_bob)
+	
+	#fov
+	var velocity_clamped = clamp(velocity.length(), 0.5, sprint_speed * 2)
+	var target_fov = base_fov + fov_change * velocity_clamped
+	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
+	
+	move_and_slide()
+
+func _headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * bob_freq) * bob_amp
+	pos.x = cos(time * bob_freq / 2) * bob_amp
+	return pos
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * sensitivity)
+		camera.rotate_x(-event.relative.y * sensitivity)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
